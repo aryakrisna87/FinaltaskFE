79190295451a01d68314e80ae18652aeed28de10
Aryakrisna87 <aryakrisna87@gmail.com>
2023-02-21 05:02:36 +0700

    FIX

--- src/pages/MyListFilm.jsx
+++ src/pages/MyListFilm.jsx
@@ -6,33 +6,37 @@ import jwt from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
 export const MyListFilm = () => {
-  const navigate = useNavigate();
-  const getToken = localStorage.getItem("token");
-  const decode = jwt(getToken);
-  let { data: transactions } = useQuery("transactionsCache", async () => {
-    const response = await API.get("/transactions");
-    return response.data.data;
-  });
-  return (
-    <div className="bg-black  min-h-screen">
-      <Navbar />
-      <h1 className="my-[5rem] ml-[3rem]">My List Film</h1>
-      <div className="flex  justify-center">
-        <div className="grid grid-cols-6 w-[70%]  gap-[2rem]  ">
-          {transactions?.map((e) => {
-            if (e.user_id === decode.id && e.status == "success") {
-              return (
-                <img
-                  onClick={() => navigate(`/DetailFilm/${e.film_id}`, { replace: true })}
-                  key={e.id}
-                  className=" w-[300px] mb-[2rem]"
-                  src={e.film.thumbnail}
-                />
-              );
-            }
-          })}
+    const navigate = useNavigate();
+    const getToken = localStorage.getItem("token");
+    const decode = jwt(getToken);
+    let { data: transactions } = useQuery("transactionsCache", async () => {
+        const response = await API.get("/transactions");
+        return response.data.data;
+    });
+    return (
+        <div className="bg-black  min-h-screen">
+            <Navbar />
+            <h1 className="pl-[8rem] my-[5rem] ml-[6rem]">My List Film</h1>
+            <div className="flex  justify-center">
+                <div className="grid grid-cols-6 w-[70%]  gap-[2rem]  ">
+                    {transactions?.map((e) => {
+                        if (e.user_id === decode.id && e.status == "success") {
+                            return (
+                                <img
+                                    onClick={() =>
+                                        navigate(`/DetailFilm/${e.film_id}`, {
+                                            replace: true,
+                                        })
+                                    }
+                                    key={e.id}
+                                    className=" w-[300px] mb-[2rem]"
+                                    src={e.film.thumbnail}
+                                />
+                            );
+                        }
+                    })}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
